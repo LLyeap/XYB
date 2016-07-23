8399107e76e1239bca20355123175697ae127dcc
LLyeap <lilei@LileideMacBook-Air.local>
2016-07-23 14:28:57 +0800

    0
    初始化分组, Icon

---
+++ XYB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XYB
+//
+//  Created by lilei on 16/7/23.
+//  Copyright © 2016年 李雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
